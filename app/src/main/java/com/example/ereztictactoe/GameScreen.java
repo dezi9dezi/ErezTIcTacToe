a0e1c90c4458a0f47991f91b2619cacfea91ff04
student <erez.shinar1@gmail.com>
2025-02-24 14:30:13 +0200

    2.24 first

--- app/src/main/java/com/example/ereztictactoe/GameScreen.java
+++ app/src/main/java/com/example/ereztictactoe/GameScreen.java
@@ -1,6 +1,10 @@
 package com.example.ereztictactoe;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +12,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameScreen extends AppCompatActivity {
+
+    RadioGroup playerSelector;
+    TextView curTurn;
+    ImageButton restart;
+    GridLayout board;
+    User user;
+    ImageButton[] cells;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,8 +29,33 @@ public class GameScreen extends AppCompatActivity {
     }
 
     private void startUI() {
-        User user = getIntent().getParcelableExtra("user");
+        user = getIntent().getParcelableExtra("user");
         String msg = user==null?"no user selected":user.toString();
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        playerSelector = findViewById(R.id.playerSelector);
+        curTurn = findViewById(R.id.turn);
+        restart = findViewById(R.id.restart);
+        restart.setOnClickListener(restart -> restartGame());
+        board = findViewById(R.id.tttBoard);
+        cells = new ImageButton[9];
+        for (int i = 0; i < cells.length; i++) {
+            ImageButton cell = new ImageButton(this);
+            cell.setLayoutParams(new GridLayout.LayoutParams(
+                    GridLayout.spec(GridLayout.UNDEFINED, 1f),
+                    GridLayout.spec(GridLayout.UNDEFINED, 1f)
+            ));
+            cell.setBackground(getDrawable(R.drawable.box_bg));
+            cell.setOnClickListener(v -> playTurn(cell));
+            cells[i] = cell;
+            board.addView(cells[i]);
+        }
+    }
+
+    private void playTurn(ImageButton cell) {
+
+    }
+
+    private void restartGame() {
     }
 }
